cc865409d792a60b7f732974c033122162f53cf8
Prageethek <146775637+Prageethek@users.noreply.github.com>
2025-06-06 12:26:36 +0530

    Implement user and product models, controllers, and config setup for MongoDB and Cloudinary

---
+++ controllers/userController.js
@@ -0,0 +1,104 @@
+import validator from "validator";
+import userModel from "../models/userModel.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const createToken = (id) =>{
+  return jwt.sign({id},process.env.JWT_SECRET)
+}
+
+
+// Route for user login
+const loginUser = async (req, res) => {
+  
+  try {
+
+    const {email,password} = req.body;
+
+    const user = await userModel.findOne({email})
+
+    if(!user){
+      return res.jsin({success:false , message:"user does not exists"})
+    }
+
+const isMatch = await bcrypt.compare(password,user.password)
+
+if(isMatch){
+  
+  const token = createToken(user._id)
+  res.json({success:true ,token})
+
+}else {
+  res.json({success:false, message:"invalid credentials"})
+}
+    
+  } catch (error) {
+    
+    console.log(error);
+    res.json({success:false,message:error.message})
+  }
+};
+
+
+
+// Route for user resgister
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // checking user already exists or not
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+  
+
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Enter valid email" });
+    }
+
+    if (password.length < 8) {
+      return res.json({ success: false, message: "Enter strong  password" });
+    }
+
+
+
+
+// hashing user password
+
+const salt = await bcrypt.genSalt(10);
+const hashedPassword = await bcrypt.hash(password,salt)
+
+const newUser = new userModel({
+  name,
+  email,
+  password:hashedPassword
+})
+
+
+
+const user = await  newUser.save()
+
+const token = createToken(user._id)
+
+res.json({success:true,token})
+
+
+    //Validation email format and strong password
+  } catch (error) {
+
+    console.log(error);
+    res.json({success:false,message:error.message})
+  }
+};
+
+
+
+//Route for admin login
+
+const adminLogin = async (req, res) => {
+  res.json({ msg: "Admin login working" });
+};
+
+export { loginUser, registerUser, adminLogin };
