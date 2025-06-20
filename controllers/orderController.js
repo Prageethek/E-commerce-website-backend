f55502b401de66183dacb348b9817829e3429eef
Prageethek <146775637+Prageethek@users.noreply.github.com>
2025-06-20 23:17:15 +0530

    Implement userOrders function to show particular user orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,5 @@
 import orderModel from "../models/orderModel.js"
+import userModel from "../models/userModel.js"
 // placing orders using COD method
 const placeOrder = async (req,res) => {
 try {
@@ -40,12 +41,31 @@ const placeOrderRazorpay = async (req,res) => {
 
 // All orders data for admin panel
 const allOrders = async (req,res) => {
+  try {
+    
+const orders = await orderModel.find({})
+res.json({success:true,orders})
+
+  } catch (error) {
+     console.log(error);
+  res.json({success:false,message:error.message})  
+  }
     
 }
 
 // User order data for frontend for a particular user 
 const userOrders = async (req,res) => {
+    try {
+      
+const {userId} = req.body
+
+const orders = await  orderModel.find({ userId })
+res.json({success:true,orders})
     
+} catch (error) {
+       console.log(error);
+  res.json({success:false,message:error.message})  
+    }
 }
 
 // update order status from Admin panel only admins can update
