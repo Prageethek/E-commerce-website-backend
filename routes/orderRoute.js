b2748a362657223eea2233a9ea7d607a6262c4cd
Prageethek <146775637+Prageethek@users.noreply.github.com>
2025-06-22 23:35:51 +0530

    implement the functionality of placing the order by Stripe

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus} from '../controllers/orderController.js'
+import {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe} from '../controllers/orderController.js'
 import adminAuth from '../middleware/adminAuth.js'
 import authUser from '../middleware/auth.js'
 
@@ -15,6 +15,9 @@ orderRouter.post('/razorpay',authUser,placeOrderRazorpay)
 
 // User feature 
 orderRouter.post('/userorder',authUser,userOrders)
+
+// verify payment
+orderRouter.post('/verifyStripe',authUser,verifyStripe)
 
 
 export default orderRouter
